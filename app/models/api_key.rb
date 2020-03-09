a7e5140872977b99b0714d872fc3a7493a83f299
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2020-03-08 20:07:03 -0400

    correction

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -3,7 +3,7 @@ class ApiKey < ApplicationRecord
 
   def self.generate_access_token
     begin
-      self.access_token = SecureRandom.hex
+      access_token = SecureRandom.hex
     end while self.class.exists?(access_token: access_token)
   end
 end
