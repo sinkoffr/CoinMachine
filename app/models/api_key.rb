d5687cc2b491dea7ad99e17a931addf4f706d9cf
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2020-03-08 20:17:04 -0400

    fixes to api_key generation

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,9 +1,11 @@
 class ApiKey < ApplicationRecord
   before_create :generate_access_token
 
-  def self.generate_access_token
+  private
+
+  def generate_access_token
     begin
-      access_token = SecureRandom.hex
-    end while self.exists?(access_token: access_token)
+      self.access_token = SecureRandom.hex
+    end while self.class.exists?(access_token: access_token)
   end
 end
